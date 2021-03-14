099c5bafc4e63c72a5de4c6197259a97f5432133
Marceloalf <59550246+Marceloalf@users.noreply.github.com>
2021-03-14 06:20:52 -0300

    Excluindo migrations desnecessarias

--- db/migrate/20210312021029_add_statu_da_consulta.rb
+++
@@ -1,5 +0,0 @@
-class AddStatuDaConsulta < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :consulta, :animal_id
-  end
-end
--- db/migrate/20210314075235_fix_add_animal_id.rb
+++
@@ -1,5 +0,0 @@
-class FixAddAnimalId < ActiveRecord::Migration[6.1]
-  def change
-    add_column :consulta, :animal_id, :integer
-  end
-end
